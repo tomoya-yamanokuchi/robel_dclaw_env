5d713a9669633ecc8d60850b9ee0154fd9308d40
tomoya-y <yamanokuchi.tomoya.ys9@is.naist.jp>
2022-10-21 10:46:33 +0900

    data_load修正

--- usecase/data_load_image.py
+++ usecase/data_load_image.py
@@ -12,11 +12,12 @@ repository = Repository(
     dataset_name = "dataset_2022102122524",
 )
 
-db_files = os.listdir(repository.dataset_save_dir)
-db_files = natsorted(db_files)
+db_files  = os.listdir(repository.dataset_save_dir)
+db_files  = natsorted(db_files)
+num_files = len(db_files)
 pprint.pprint(db_files)
 
-for db in db_files:
+for index, db in enumerate(db_files):
     # import ipdb; ipdb.set_trace()
     db_name, suffix = db.split(".")
     repository.open(filename=db_name)
@@ -24,7 +25,7 @@ for db in db_files:
     img_ran = repository.repository["image"].random_nonfix
     step, width, height, channel = img_can.shape
     for t in range(step):
-        print("() [{}] step: {}".format(db, t))
+        print("({}/{}) [{}] step: {}".format(index+1, num_files, db, t))
         cv2.imshow('img', np.concatenate((img_ran[t], img_can[t]), axis=1))
         cv2.waitKey(10)
         # print(repository.repository["state"].robot_position)
